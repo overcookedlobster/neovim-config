eb824315cb8dafc396fe5ceaa0ea48e8e4e6c2c4
flyinglobster <flyinglobster@pop-os.localdomain>
2024-08-12 03:28:46 +0700

    plus keybind

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -148,6 +148,27 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.defer_fn(function()
   local cmp = require('cmp')
   cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<Tab>'] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end,
+      ['<S-Tab>'] = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end,
+    }),
     sources = {
       { name = 'omni' },
       -- Add other sources you're using
